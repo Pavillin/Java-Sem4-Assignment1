1a90d49e1598191cc371ea00f1aca314888e8e71
Pavillin <dylansprague11@hotmail.com>
2018-10-06 20:32:27 -0400

    Added Contact validation

--- src/Models/Contact.java
+++ src/Models/Contact.java
@@ -1,12 +1,12 @@
 package Models;
 
-import java.util.Date;
+import java.time.LocalDate;
 
 public class Contact {
     private String firstName, lastName, address, phone;
-    private Date birthday;
+    private LocalDate birthday;
 
-    public Contact(String firstName, String lastName, String address, String phone, Date birthday) {
+    public Contact(String firstName, String lastName, String address, String phone, LocalDate birthday) {
         setFirstName(firstName);
         setLastName(lastName);
         setAddress(address);
@@ -19,7 +19,11 @@ public class Contact {
     }
 
     public void setFirstName(String firstName) {
-        this.firstName = firstName;
+        if (firstName.isEmpty()){
+            throw new IllegalArgumentException("First name cannot be empty");
+        } else{
+            this.firstName = firstName;
+        }
     }
 
     public String getLastName() {
@@ -27,7 +31,11 @@ public class Contact {
     }
 
     public void setLastName(String lastName) {
-        this.lastName = lastName;
+        if (lastName.isEmpty()){
+            throw new IllegalArgumentException("Last name cannot be empty");
+        } else{
+            this.lastName = lastName;
+        }
     }
 
     public String getAddress() {
@@ -35,7 +43,11 @@ public class Contact {
     }
 
     public void setAddress(String address) {
-        this.address = address;
+        if (address.isEmpty()){
+            throw new IllegalArgumentException("Address cannot be empty");
+        } else{
+            this.address = address;
+        }
     }
 
     public String getPhone() {
@@ -43,14 +55,24 @@ public class Contact {
     }
 
     public void setPhone(String phone) {
-        this.phone = phone;
+        if (phone.isEmpty()){
+            throw new IllegalArgumentException("Phone Number cannot be empty");
+        } else{
+            this.phone = phone;
+        }
     }
 
-    public Date getBirthday() {
+    public LocalDate getBirthday() {
         return birthday;
     }
 
-    public void setBirthday(Date birthday) {
-        this.birthday = birthday;
+    public void setBirthday(LocalDate birthday){
+        if (birthday.isAfter(LocalDate.now())){
+            throw new IllegalArgumentException("Birthday cannot be in the future");
+        } else if (birthday.isEqual(LocalDate.now())){
+            throw new IllegalArgumentException("You were not born today, and neither was I");
+        } else{
+            this.birthday = birthday;
+        }
     }
 }
